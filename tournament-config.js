ad9d0c56321ee616262361f23c53c1dd836ba2b8
PepMaestro <maestrocup@zohomail.eu>
2026-08-08 00:51:42 +0200

    Update tournament-config.js

--- tournament-config.js
+++ tournament-config.js
@@ -1,5 +1,5 @@
 // Le seul endroit à changer pour switcher de format de tournoi.
-export const MODE = "32"; // "32" ou "16"
+export const MODE = "16"; // "32" ou "16"
 
 const POULES_32 = ["A", "B", "C", "D", "E", "F", "G", "H"];
 const POULES_16 = ["A", "B", "C", "D"];
